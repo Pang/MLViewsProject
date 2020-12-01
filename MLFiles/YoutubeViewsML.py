0d2e23344f01cab241c213478792ba97ddccf39f
cPango <ryan.beckett@outlook.com>
2020-12-01 21:16:15 +0000

    first commit

---
+++ MLFiles/YoutubeViewsML.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 21:00:16 2020
+
+@author: Pang
+"""
+
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+
+dataset = pd.read_csv('../YoutubeStatData/USvideos.csv')
+
