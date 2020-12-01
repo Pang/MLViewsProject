5b7017bc76a60191ba74aee6ef0974ee18f46383
cPango <ryan.beckett@outlook.com>
2020-12-01 23:57:12 +0000

    more data cleaning

--- MLFiles/YoutubeViewsML.py
+++ MLFiles/YoutubeViewsML.py
@@ -9,9 +9,32 @@ import numpy as np
 import matplotlib as plt
 import pandas as pd
 
-dataset = pd.read_csv('../YoutubeStatData/USvideos.csv')
+dataset = pd.read_csv('../CleanedData/USvideosClean.csv')
 
-x = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+x = dataset.iloc[:, 1:].values #features
+y = dataset.iloc[:, 0].values #dependent variable (views)
+
+print(x[0])
+print("--------------------------------")
+
+
+""" *Model could assume higher/lower ids affect outcome
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='drop')
+x = np.array(ct.fit_transform(x))
 
 print(x)
+
+"""
+
+"""
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+
+# Standardisation feature scaling on views
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:, 0] = sc.fit_transform(x_train[:, 0])
+"""
--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # MLViewsProject
 
-To report what impact a video title, tags, genre, likes/dislikes and description have on the views of a video. (A thanks to Kaggle.com for the dataset)
+To report what impact a video title, tags and category have on the views of a video when trending. (A thanks to Kaggle.com for the dataset)
 
 Columns I have removed from the original file that I thought may not have an affect on views are:
-- videoId
+- video_id
 - comments_disabled
 - ratings_disabled
 - video_error_or_removed
-- Maybe thumbnail, unless I can find a way to scan the img inside the links for more info
+- likes
+- dislikes
+- comment_count
+- publish_time
+- trending_date
+- channel_title (since channels could bias the data, also confusing the model when making a prediction on unknown channels)
+- thumbnail_link (This could probably be a seperate ML algorithm in itself)
