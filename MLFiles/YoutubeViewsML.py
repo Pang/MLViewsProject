dfe2ef82ef2f5498e0db199f53e175ee14b185ad
cPango <ryan.beckett@outlook.com>
2020-12-02 22:43:53 +0000

    naivebayes multinomial

--- MLFiles/YoutubeViewsML.py
+++ MLFiles/YoutubeViewsML.py
@@ -6,35 +6,25 @@ Created on Tue Dec  1 21:00:16 2020
 """
 
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv('../CleanedData/USvideosClean.csv')
 
-x = dataset.iloc[:, 1:].values #features
-y = dataset.iloc[:, 0].values #dependent variable (views)
-
-print(x[0])
-print("--------------------------------")
+x = dataset.iloc[:, 2].values #features
+y = dataset.iloc[:, 0].values #dependent variable / label (views)
 
 
-""" *Model could assume higher/lower ids affect outcome
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='drop')
-x = np.array(ct.fit_transform(x))
+from sklearn.feature_extraction.text import CountVectorizer  
+cv = CountVectorizer()
+xVector = cv.fit_transform(x)
 
-print(x)
 
-"""
-
-"""
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+x_train, x_test, y_train, y_test = train_test_split(xVector, y, test_size = 0.2, random_state = 1)
 
+from sklearn.naive_bayes import MultinomialNB
+nv = MultinomialNB()
+nv.fit(x_train, y_train)
 
-# Standardisation feature scaling on views
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-x_train[:, 0] = sc.fit_transform(x_train[:, 0])
-"""
+print(nv.score(x_test, y_test))
--- README.md
+++ README.md
@@ -1,16 +1,7 @@
 # MLViewsProject
 
 To report what impact a video title, tags and category have on the views of a video when trending. (A thanks to Kaggle.com for the dataset)
+Rows: 40949
 
-Columns I have removed from the original file that I thought may not have an affect on views are:
-- video_id
-- comments_disabled
-- ratings_disabled
-- video_error_or_removed
-- likes
-- dislikes
-- comment_count
-- publish_time
-- trending_date
-- channel_title (since channels could bias the data, also confusing the model when making a prediction on unknown channels)
-- thumbnail_link (This could probably be a seperate ML algorithm in itself)
+For my first test, I have only used the video title as the feature to predict the views. This was with a test_size of 0.2 and a multinomial classifier.
+I was let down to see a 0.0007326007326007326% accuracy when predicting but feel this may be improved with a regressional approach.
